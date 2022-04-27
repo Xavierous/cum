bea90ce7014d23a31cff9a53e1c12d1696fb7cc8
Xavierous <102414837+Xavierous@users.noreply.github.com>
2022-04-27 20:13:51 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("run2").lisensi().folder()
+       __import__("run5").lisensi().folder()
    except Exception as e:
        exit(str(e))
